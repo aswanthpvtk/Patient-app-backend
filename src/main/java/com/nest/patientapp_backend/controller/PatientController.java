5485775038253a7b931d1a29ae9a1366923180d0
Aswanth P <Aswanth@NTP-LAP-734.chn.nestgroup.net>
2022-12-07 01:30:26 +0530

    pat

--- src/main/java/com/nest/patientapp_backend/controller/PatientController.java
+++ src/main/java/com/nest/patientapp_backend/controller/PatientController.java
@@ -3,21 +3,30 @@ package com.nest.patientapp_backend.controller;
 import com.nest.patientapp_backend.dao.PatientDao;
 import com.nest.patientapp_backend.model.Patient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class PatientController {
 
     @Autowired
     private PatientDao dao;
-
+@CrossOrigin(origins = "*")
      @PostMapping(path = "/add",consumes = "application/json",produces = "application/json")
     public String PatientAdd(@RequestBody Patient p)
       {
+
+          System.out.println(p.getDate().toString());
           dao.save(p);
     return "add patient";
+      }
+
+    @CrossOrigin(origins = "*")
+      @GetMapping("/view")
+      public List<Patient> View()
+      {
+          return (List<Patient>) dao.findAll();
       }
 
 }
